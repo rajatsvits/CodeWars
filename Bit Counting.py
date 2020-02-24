8528fff38b6c2dd63410c58b42b6c2198eb24fb2
Rajat Mahajan <rajatmahajansvits@gmail.com>
2020-02-24 15:53:42 +0530

    Create Bit Counting.py

---
+++ Bit Counting.py
@@ -0,0 +1,14 @@
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+def countBits(x):
+    c=0
+    n=x
+    while n>0:
+        if n%2==1:
+            c+=1
+        n=int(n/2)
+#         print(n,c)
+    return c
+#     return [count(c) if x==1 while n%2==1: n=n/2]
