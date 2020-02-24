72d5df3475875c4e997100361fe538b39c32a7f9
Rajat Mahajan <rajatmahajansvits@gmail.com>
2020-02-24 14:23:22 +0530

    Update Friend or Foe?.py

--- Friend or Foe?.py
+++ Friend or Foe?.py
@@ -1,13 +1,13 @@
-Make a program that filters a list of strings and returns a list with only your friends name in it.
+# Make a program that filters a list of strings and returns a list with only your friends name in it.
 
-If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+# If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
 
-Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+# Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
 
-i.e.
+# i.e.
 
-friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
-Note: keep the original order of the names in the output.
+# friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+# Note: keep the original order of the names in the output.
 
 #mine
 def friend(x):
